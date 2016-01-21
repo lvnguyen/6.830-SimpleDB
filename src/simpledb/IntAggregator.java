9398433c0aa9e7633409157a5aa972bc06b0030a
Linh V. Nguyen <lvnguyen@mit.edu>
2016-01-21 18:46:07 -0500

    Implement IntAggregator

--- src/simpledb/IntAggregator.java
+++ src/simpledb/IntAggregator.java
@@ -1,20 +1,63 @@
 package simpledb;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Knows how to compute some aggregate over a set of IntFields.
  */
 public class IntAggregator implements Aggregator {
-
+	
+	private int m_gbfield;
+	private Type m_gbtype;
+	private int m_afield;
+	private Op m_op;
+	
+	HashMap<Field, Integer> m_aggregateData;
+	HashMap<Field, Integer> m_count;
+	TupleDesc m_td;
+	
+	private TupleDesc createTupleDesc() {
+		String[] fieldNames;
+		Type[] fieldTypes;
+		
+		if (m_gbfield == NO_GROUPING) {
+			fieldNames = new String[] {"aggregate values"};
+			fieldTypes = new Type[] {Type.INT_TYPE};
+		}
+		else {
+			fieldNames = new String[] {"groupby values", "aggregate values"};
+			fieldTypes = new Type[] {m_gbtype, Type.INT_TYPE};
+		}
+		return new TupleDesc(fieldTypes, fieldNames);
+	}
+	
     /**
-     * Aggregate constructor
      * @param gbfield the 0-based index of the group-by field in the tuple, or NO_GROUPING if there is no grouping
      * @param gbfieldtype the type of the group by field (e.g., Type.INT_TYPE), or null if there is no grouping
+     * Aggregate constructor
      * @param afield the 0-based index of the aggregate field in the tuple
      * @param what the aggregation operator
      */
 
     public IntAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
-        // some code goes here
+    	// some code goes here
+    	m_gbfield = gbfield;
+    	m_gbtype = gbfieldtype;
+    	m_afield = afield;
+    	m_op = what;
+    	m_aggregateData = new HashMap<Field, Integer>();
+    	m_count = new HashMap<Field, Integer>();    	
+    	m_td = createTupleDesc();
+    }
+    
+    private int setInitData() {
+    	switch (m_op) {
+    		case MIN: return Integer.MAX_VALUE;
+    		case MAX: return Integer.MIN_VALUE;
+    		case SUM: case COUNT: case AVG: return 0;
+    		default: throw new IllegalArgumentException();
+    	}
     }
 
     /**
@@ -23,6 +66,39 @@ public class IntAggregator implements Aggregator {
      */
     public void merge(Tuple tup) {
         // some code goes here
+    	Field key = (m_gbfield == NO_GROUPING) ? null : tup.getField(m_gbfield);
+    	if (!m_aggregateData.containsKey(key)) {
+    		m_aggregateData.put(key, setInitData());
+    		m_count.put(key, 0);
+    	}
+    	 
+    	int tup_value = ((IntField) tup.getField(m_afield)).getValue();
+    	int cur_value = m_aggregateData.get(key);
+    	int count_value = m_count.get(key);
+    	
+    	switch (m_op) {
+    	case MIN:
+    		cur_value = Math.min(cur_value, tup_value);
+    		break;
+    	case MAX:
+    		cur_value = Math.max(cur_value, tup_value);
+    		break;
+    	case SUM:
+    		cur_value += tup_value;
+    		break;
+    	case COUNT:
+    		count_value++;
+    		break;
+    	case AVG:
+    		count_value++;
+    		cur_value += tup_value;
+    		break;
+    	default:
+    		throw new IllegalArgumentException();
+    	}
+    	
+    	m_aggregateData.put(key, cur_value);
+    	m_count.put(key, count_value);
     }
 
     /**
@@ -35,7 +111,36 @@ public class IntAggregator implements Aggregator {
      */
     public DbIterator iterator() {
         // some code goes here
-        throw new UnsupportedOperationException("implement me");
+        ArrayList<Tuple> tuples = new ArrayList<Tuple>();
+        for (Field key : m_aggregateData.keySet()) {
+        	Tuple nextTuple = new Tuple(m_td);
+        	int recvValue;
+        	
+        	switch (m_op) {
+        	case MIN: case MAX: case SUM:
+        		recvValue = m_aggregateData.get(key);
+        		break;
+        	case COUNT:
+        		recvValue = m_count.get(key);
+        		break;
+        	case AVG:
+        		recvValue = m_aggregateData.get(key) / m_count.get(key);
+        		break;
+        	default:
+        		recvValue = setInitData();  // shouldn't reach here
+        	}
+        	
+        	Field recvField = new IntField(recvValue);
+        	if (m_gbfield == NO_GROUPING) {
+        		nextTuple.setField(0, recvField);
+        	}
+        	else {
+        		nextTuple.setField(0, key);
+        		nextTuple.setField(1, recvField);
+        	}
+        	tuples.add(nextTuple);
+        }
+        return new TupleIterator(m_td, tuples);
     }
 
 }
